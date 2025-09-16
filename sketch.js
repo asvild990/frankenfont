daaef12af1034a0dd087feb6e72150315a1a8e30
asvild990 <48067245+asvild990@users.noreply.github.com>
2025-09-16 02:07:54 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -120,7 +120,7 @@ function windowResized() {
 }
 
 function autoRandomize() {
-  let count = floor(random(10, 24)); // change 3–5 cells per interval
+  let count = floor(random(24, 36)); // change n cells per interval
   for (let n = 0; n < count; n++) {
     let i = floor(random(cols));
     let j = floor(random(rows));
