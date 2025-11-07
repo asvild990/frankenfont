daa39c8b1330a5c1792f7602d66577ae39c5b533
asvild990 <48067245+asvild990@users.noreply.github.com>
2025-11-06 22:54:28 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function initTileCache() {
 
 function getTextHeight() {
   textFont("Futura");
-  textSizeValue = canvasW * 0.15;
+  textSizeValue = canvasW * 0.12;
   textSize(textSizeValue);
 }
 
