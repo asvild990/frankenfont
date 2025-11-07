a7480940a9cefb8514d6e50de35df69903caf3d7
asvild990 <48067245+asvild990@users.noreply.github.com>
2025-11-06 22:39:23 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let fonts = ["Futura", "Didot", "Verdana", "Baskerville", "Avenir", "Gill Sans", "Source Code Pro", "Cooper", "Helvetica", "Rockwell"];
-let message = "Form &\nFunction";
+let message = "Making Things\nMake Sense";
 let cols = 24;
 let rows = 18;
 let fontGrid = [];
